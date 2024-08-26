203aa6379d4e61d38137c3ffeb4ff6e97bc0f0b6
fernanda1417 <135229119+fernanda1417@users.noreply.github.com>
2024-08-26 09:32:00 -0300

    ..

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -7,77 +9,70 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Você costuma fumar",
-        alternativas:[
+        alternativas: [
             {
-                texto:"Sim",
-                    afirmação: "afirmação"
+                texto: "Sim",
+                afirmação: "È vicio antigo. "
+            },
+            {
+                texto: "Não",
+                afirmação: "Isso faz mal a saude. "
+            }
+        ]
     },
-    {
-        texto:"Não",
-            afirmação:"afirmação"
-},
-]  
-    },    
     {
         enunciado: "Você gosta de estudar?",
-        alternativas:[
+        alternativas: [
             {
-                texto:"Sim",
-                    afirmação: "afirmação"
+                texto: "Sim ",
+                afirmação: "Para ter um futuro. "
+            },
+            {
+                texto: "Não ",
+                afirmação: "Não me leva a nada. "
+            }
+        ]
     },
     {
-        texto:"Não",
-            afirmação:"afirmação"
-},
-]  
-    },  
-    {
-        enunciado: "Você costuma ter uma boa noite de sono?",
-        alternativas:[
+        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
             {
-                texto:"Sim",
-                    afirmação: "afirmação"
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmação: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmação: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+            }
+        ]
     },
     {
-        texto:"Não",
-            afirmação:"afirmação"
-},
-]  
-    },  
-    {
-        enunciado: "Você gosta de socializar?",
-        alternativas:[
+        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        alternativas: [
             {
-                texto:"Sim",
-                    afirmação: "afirmação"
+                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
+                afirmação: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+            },
+            {
+                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
+                afirmação: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+            }
+        ]
     },
     {
-        texto:"Não",
-            afirmação:"afirmação"
-},
-]  
-    },  
-    {
-        enunciado: "Você costuma sair?",
-        alternativas:[
+        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        alternativas: [
             {
-                texto:"Sim",
-                    afirmação: "afirmação"
+                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
+                afirmação: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+            },
+            {
+                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                afirmação: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+            }
+        ]
     },
-    {
-        texto:"Não",
-            afirmação:"afirmação"
-},
-]  
-    },  
 ];
-
-
-
-
-
-
-
 
 
 let atual = 0;
@@ -96,12 +91,12 @@ function mostraPergunta() {
 }
 
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas) {
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-
-     caixaAlternativas.appendChild(botaoAlternativas);
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
 
 function respostaSelecionada(opcaoSelecionada) {
@@ -118,4 +113,3 @@ function mostraResultado() {
 }
 
 mostraPergunta();
-}
